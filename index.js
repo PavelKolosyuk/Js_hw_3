11cdcdd5642a44c8b1ea9090dfa5da06218f6067
p.kolosyuk@ukr.net <p.kolosyuk@ukr.net>
2019-03-06 12:14:26 +0200

    done

--- index.js
+++ index.js
@@ -56,4 +56,3 @@ console.log(addLogin(logins, 'robotGoogles')); // 'Такой логин уже 
 console.log(addLogin(logins, 'Zod')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
 console.log(addLogin(logins, 'jqueryisextremelyfast')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
 console.log(logins);
-
